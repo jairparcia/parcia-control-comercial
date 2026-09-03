8187e7494de698f0e9dbe2972d89903e1c5ee31e
Carlos Molina <carlos@parcia.co>
2026-09-03 03:19:23 -0600

    [CC-11] Persist transactions in local DB with Stripe import

--- resources/views/livewire/admin/transactions-component.blade.php
+++ resources/views/livewire/admin/transactions-component.blade.php
@@ -18,7 +18,11 @@
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"/>
                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"/>
             </svg>
+<<<<<<< HEAD
             {{ __('common.import_stripe') }}
+=======
+            Import from Stripe
+>>>>>>> 6a859ec ([CC-11] Persist transactions in local DB with Stripe import)
         </button>
     </div>
 
