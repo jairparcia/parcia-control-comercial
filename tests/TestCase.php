904cfb232c51e82a3b48e175929222cf4053b70a
Carlos Molina <carlos@parcia.co>
2026-09-04 03:47:13 -0600

    [CC-12] Setup Localization

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,13 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\App;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        App::setLocale('es');
+    }
 }
